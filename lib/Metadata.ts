7bf8f43f2f54d657facb037c6723472594c57c47
Nwosa <obere4u@gmail.com>
2024-12-16 12:58:52 +0100

    style: Improve animation

--- lib/Metadata.ts
+++ lib/Metadata.ts
@@ -31,7 +31,8 @@ export function generateMetadata({
     openGraph: {
       title: fullTitle,
       description: description,
-      url: `https://founders-friday-phi.vercel.app${path}`.replace(/\/+$/, ""),
+      url: `https://founders-friday-phi.vercel.app${path || "/"}`,
+
       siteName: baseTitle,
       images: [
         {
